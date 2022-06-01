3274775882539606d4d14bd90894ba7618466222
Morris Wang <morris1214@gmail.com>
2022-06-01 02:13:56 +0000

    Completed javascript-primitives-and-variables exercise

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,20 @@
+var fullName = 'Morris Wang';
+console.log('full name:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var isCool = true;
+console.log('I am cool:', isCool);
+console.log('typeof isCool:', typeof isCool);
+
+var totalPets = 1;
+console.log('total pets:', totalPets);
+console.log('typeof totalPets:', typeof totalPets);
+
+var unknown = null;
+console.log('unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never =
+// eslint-disable-next-line no-use-before-define
+console.log('never:', never);
+console.log('typeof never:', typeof never);
