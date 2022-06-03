5a6fde29180ea8b06c543947b5adb81b7edfa969
Morris Wang <morris1214@gmail.com>
2022-06-03 03:24:30 +0000

    Completed javascript-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,38 @@
+var orderHistory = [
+  {
+    orderNumber: '114-3941689-8772232',
+    itemName: 'Javascript for impatient programmers',
+    price: '$31.55',
+    shipping: 'JS Masher',
+    orderDate: 'August 4, 2020',
+    deliveryDate: 'Aug 8, 2020',
+    returnByDate: 'Sep 7, 2020'
+  },
+  {
+    orderNumber: '113-9984268-1280257',
+    itemName: 'The Timeless Way of Building',
+    price: '$41.33',
+    shipping: 'JS Masher',
+    orderDate: 'July 20, 2020',
+    deliveryDate: 'Jul 20,2020',
+    returnByDate: 'Aug 19, 2020'
+  },
+  {
+    orderNumber: '114-2875557-9059409',
+    itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    price: '$15.98',
+    shipping: 'JS Masher',
+    orderDate: 'July 4, 2020',
+    deliveryDate: 'Jul 7, 2020',
+    returnByDate: 'Aug 5, 2020'
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    itemName: ['Gamecube Controller- Super Smash Bros. Edition (Nintendo Switch)', 'The Art of Sql'],
+    price: ['$15.98', '$33.99'],
+    shipping: 'JS Masher',
+    orderDate: 'July 4, 2020',
+    deliveryDate: 'Jul 7, 2020',
+    returnByDate: 'Aug 5, 2020'
+  }
+];
