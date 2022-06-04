de4adca86d4ce247f5910c1c803d8a35ae38d845
Morris Wang <morris1214@gmail.com>
2022-06-04 04:40:25 +0000

    Completed javascript-functions exercise

---
+++ javascript-functions/main.js
@@ -0,0 +1,39 @@
+function convertMinutesToSeconds(minutes) {
+  var totalSeconds = minutes * 60;
+  return totalSeconds;
+}
+
+var totalSecondsResult = convertMinutesToSeconds(5);
+console.log('totalSecondsResult:', totalSecondsResult);
+
+function greet(name) {
+  var greeting = 'Hey ' + name;
+  return greeting;
+}
+
+var greetingResult = greet('Beavis');
+console.log('greetingResult:', greetingResult);
+
+function getArea(width, height) {
+  var totalArea = width * height;
+  return totalArea;
+}
+
+var totalAreaResult = getArea(17, 42);
+console.log('totalAreaResult:', totalAreaResult);
+
+function getFirstName(person) {
+  var personFirstName = person.firstName;
+  return personFirstName;
+}
+
+var personFirstNameResult = getFirstName({ firstName: 'Lelouche', lastName: 'Lamperouge' });
+console.log('personFirstNameResult:', personFirstNameResult);
+
+function getLastElement(array) {
+  var lastElement = array.length - 1;
+  return lastElement;
+}
+
+var lastElementResult = getLastElement(['propane', 'and', 'propane', 'accessories']);
+console.log('lastElementResult:', lastElementResult);
