fb95f1cb5246e9800bc93de5e2d0fef4aa0189c2
Morris Wang <morris1214@gmail.com>
2022-06-15 05:15:59 +0000

    completed object manipulation-1 exercise

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,16 @@
+/* exported getValue */
+// define a function named getValue that
+//   takes 2 parameters, an object and key -completed
+// Use bracket notation with the parameters to find the value
+//   return value of object at the key -completed
+
+function getValue(object, key) {
+  var result = object[key];
+  return result;
+}
+
+// define a function named getValue that
+//   takes 2 parameters, an object and key
+// Use bracket notation with the parameters to find the value
+//   assign that to a result variable
+//     return the result variable
